206d7d1760eb5f7f5aa0e4dd7d891e2ed6f16320
leumassopmac53 <leumassopmac53@gmail.com>
2026-08-17 21:07:41 -0300

    Add files via upload

---
+++ js/sistem-animacao.js
@@ -0,0 +1,32 @@
+function animar(e, delta, img) {
+  const animacao = e.animacoes[e.anim];   
+  if (delta - animacao.ultimoTempo >= animacao.intervalo) {
+
+    img.src = animacao.sprites[animacao.frame];
+
+    animacao.frame++;
+
+    if (animacao.frame >= animacao.sprites.length) {
+      if (animacao.repeat) {
+        animacao.frame = 0;
+      } else {
+        animacao.frame = animacao.sprites.length -1
+      }
+    }
+
+    animacao.ultimoTempo = delta
+  }
+}
+
+function mudarAnim(e,anim){
+  e.animacoes[e.anim].frame = 0
+  e.anim = anim
+}
+
+function loop(delta){
+
+  animar(gato,delta,gato.img)
+  requestAnimationFrame(loop)
+}
+
+requestAnimationFrame(loop)
